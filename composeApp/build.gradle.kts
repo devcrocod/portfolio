e63fc0beeca945ae3a8f8c0239e7ecf6a722eced
devcrocod <devcrocod@gmail.com>
2026-05-14 16:57:05 +0200

    Remove JVM-specific implementations and desktop target configuration

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,16 +1,12 @@
-import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
-    id("org.jetbrains.compose.hot-reload")
 }
 
 kotlin {
-    jvm()
-
     @OptIn(ExperimentalWasmDsl::class)
     wasmJs {
         outputModuleName = "composeApp"
@@ -36,20 +32,6 @@ kotlin {
             implementation(libs.haze.blur)
             implementation(libs.haze.blur.materials)
             implementation(libs.kotlinx.datetime)
-        }
-        jvmMain.dependencies {
-            implementation(compose.desktop.currentOs)
-        }
-    }
-}
-
-compose.desktop {
-    application {
-        mainClass = "io.github.devcrocod.MainKt"
-        nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "Portfolio"
-            packageVersion = "1.0.0"
         }
     }
 }
